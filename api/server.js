5ed81cadedd6bbbe68ee0c01738395c6a796e4b5
christiano39 <carneson39@gmail.com>
2020-08-22 14:39:25 -0600

    added restricted auth middleware

--- api/server.js
+++ api/server.js
@@ -5,13 +5,18 @@ const helmet = require("helmet");
 const usersRoutes = require("../users/usersRoutes");
 const authRoutes = require("../auth/authRoutes");
 
+const {
+  requiresToken,
+  requiresAdmin,
+} = require("../restricted/restrictedMiddleware");
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use("/api/users", usersRoutes);
+server.use("/api/users", requiresToken, requiresAdmin, usersRoutes);
 server.use("/api/auth", authRoutes);
 
 module.exports = server;

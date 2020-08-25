700c63163c94408a7b858fd59f594d6f47879b40
Khalil <khalilfoulks@gmail.com>
2020-08-25 16:28:30 -0400

    finished testing users router

--- __tests__/how-to.test.js
+++ __tests__/how-to.test.js
@@ -2,13 +2,18 @@ const request = require("supertest");
 const server = require("../api/server");
 const db = require("../data/dbConfig")
 
-describe("auth router", () => {
+describe("how-to router", () => {
     beforeEach(async () =>{
         await db("posts").truncate();
         await db("users").truncate();
     })
 
-    describe("HTTPmethodhere routehere", () => {
-        
+    describe("HTTPmethodhere /howto", () => {
+        it("should send a 401", async () => {
+            await request(server).get("/api/howto")
+            .then(res => {
+                expect(res.status).toBe(401);
+            })
+        })
     })
 })
--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,12 +2,151 @@ const request = require("supertest");
 const server = require("../api/server");
 const db = require("../data/dbConfig")
 
-describe("auth router", () => {
+describe("users router", () => {
+    
     beforeEach(async () =>{
         await db("users").truncate();
-    })
 
-    describe("HTTPmethodhere routehere", () => {
-        
+        await request(server).post("/api/auth/register").send({
+            username:"user1",
+            password:"pass"
+        });
+        await request(server).post("/api/auth/register").send({
+            username:"user2",
+            password:"pass"
+        });
+    });
+
+
+    describe("GET /users", () => {
+        it("should return a list of users", async () => {
+            await request(server)
+            .post("/api/auth/register").send({
+            username:"jest25",
+            password:"hashedpass"
+            })
+
+            await request(server)
+            .post("/api/auth/login").send({
+                username:"jest25",
+                password:"hashedpass"
+            })
+            .then(res => {
+                token = res.body.token
+            })
+
+            await request(server)
+            .get("/api/users")
+            .set('Authorization', token)
+            .then(res => {
+                expect(res.body).toHaveLength(3);
+            })
+        })
+        it("should send a 401", async () => {
+            await request(server).get("/api/users")
+            .then(res => {
+                expect(res.status).toBe(401);
+            })
+        })
+        it("should send a 200", async () => {
+            await request(server)
+            .post("/api/auth/login").send({
+                username:"user2",
+                password:"pass"
+            })
+            .then(res => {
+                token = res.body.token
+            })
+
+            await request(server)
+            .get("/api/users")
+            .set('Authorization', token)
+            .then(res => {
+                expect(res.status).toBe(200);
+            })
+        })
+        it("should be defined", async () => {
+            await request(server).get("/api/users")
+            .then(res => {
+                expect(res.body).toBeDefined();
+            })
+        })
+    })
+    describe("GET /users/:id", () => {
+        it("should return a single user", async () => {
+            await request(server)
+            .post("/api/auth/login").send({
+                username:"user2",
+                password:"pass"
+            })
+            .then(res => {
+                token = res.body.token
+            })
+
+            await request(server)
+            .get("/api/users/2")
+            .set('Authorization', token)
+            .then(res => {
+                expect([res.body]).toHaveLength(1);
+            })
+        })
+        it("should return the correct id", async () => {
+            await request(server)
+            .post("/api/auth/login").send({
+                username:"user2",
+                password:"pass"
+            })
+            .then(res => {
+                token = res.body.token
+            })
+
+            await request(server)
+            .get("/api/users/2")
+            .set('Authorization', token)
+            .then(res => {
+                const expected = {id: 2}
+                expect(res.body).toMatchObject(expected)
+            })
+        })
+        it("should send a 401", async () => {
+            await request(server).get("/api/users/2")
+            .then(res => {
+                expect(res.status).toBe(401);
+            })
+        })
+        it("should send a 200", async () => {
+            await request(server)
+            .post("/api/auth/login").send({
+                username:"user2",
+                password:"pass"
+            })
+            .then(res => {
+                token = res.body.token
+            })
+
+            await request(server)
+            .get("/api/users/2")
+            .set('Authorization', token)
+            .then(res => {
+                expect(res.status).toBe(200);
+            })
+        })
+        it("should send json", async () => {
+            await request(server)
+            .post("/api/auth/login").send({
+                username:"user2",
+                password:"pass"
+            })
+            .then(res => {
+                token = res.body.token
+            })
+
+            await request(server)
+            .get("/api/users/2")
+            .set('Authorization', token)
+            .then(res => {
+                expect(res.type).toMatch(/json/i);
+            })
+        })
     })
 })

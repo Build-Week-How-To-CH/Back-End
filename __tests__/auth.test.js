81b5b366af60a77298e45cddd87fb9ee758c6a8f
Khalil <khalilfoulks@gmail.com>
2020-08-25 13:49:27 -0400

    finished testing auth router

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -38,5 +38,85 @@ describe("auth router", () => {
                     expect(res.type).toMatch(/json/i)
                 })
         })
+        it("should send a 400 if no username or password are sent", async () => {
+
+            const res = await request(server)
+            .post("/api/auth/register").send({
+                username:"",
+                password:""
+            })
+    
+            expect(res.status).toBe(400);
+        })
+        it("should not contain an unhashed password", async () => {
+            await request(server).post("/api/auth/register").send({
+                username:"jest25",
+                password:"hashedpass"
+                })
+                .then(res => {
+                    const expected = {password: 'hashedpass'}
+                    expect(res.body.data).not.toMatchObject(expected)
+                })
+            })
+    })
+
+    describe("POST /login", () => {
+        it("should contain a message", async () => {
+            await request(server)
+                .post("/api/auth/register").send({
+                username:"jest25",
+                password:"hashedpass"
+                })
+
+            await request(server)
+                .post("/api/auth/login").send({
+                    username:"jest25",
+                    password:"hashedpass"
+                })
+                .then(res => {
+                    const expected = {message: "welcome to the API"}
+                    expect(res.body).toMatchObject(expected)
+                })
+        })
+        it("should send a 200", async () => {
+
+            await request(server).post("/api/auth/register").send({
+                username:"jest25",
+                password:"hashedpass"
+            });
+
+            const res = await request(server)
+            .post("/api/auth/login").send({
+                username:"jest25",
+                password:"hashedpass"
+            })
+    
+            expect(res.status).toBe(200);
+        })
+        it("should send a 401 if password doesn't match", async () => {
+
+            await request(server).post("/api/auth/register").send({
+                username:"jest25",
+                password:"hashedpass"
+            });
+
+            const res = await request(server)
+            .post("/api/auth/login").send({
+                username:"jest25",
+                password:"pssword"
+            })
+    
+            expect(res.status).toBe(401);
+        })
+        it("should send a 400 if no username or password are sent", async () => {
+
+            const res = await request(server)
+            .post("/api/auth/login").send({
+                username:"",
+                password:""
+            })
+    
+            expect(res.status).toBe(400);
+        })
     })
 })
